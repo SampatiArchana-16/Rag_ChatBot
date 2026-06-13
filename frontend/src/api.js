9130259e1449462f3c14b0d605f50248f53d24e4
SampatiArchana-16 <sampatiarchana16@gmail.com>
2026-06-13 16:02:11 +0530

    connect render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,17 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000",
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL:
+    "https://rag-chatbot-y5t0.onrender.com"
 });
 
 export default API;
